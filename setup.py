b9026213f158fb873d7fee796ea912cab7695bba
Momo <30694397+paris18e@users.noreply.github.com>
2017-08-08 23:47:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
         'matplotlib',
     ],
     packages=['shapelets'],
-    package_dir={'': '/Users/Mo/anaconda/lib/python3.6/site-packages/shaplets-python'}
+    package_dir={'': 'lib/python3.6/site-packages/shaplets-python'}
 )
